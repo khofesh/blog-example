86d8b4dc18b07cd90cb69d3f1b8c2285a70b3f7f
fahmi ahmad <fahmiahmad@tutanota.com>
2021-11-19 10:20:03 +0700

    event bus

---
+++ event-bus/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.post("/events", (req, res) => {
+  const event = req.body;
+
+  axios.post("http://localhost:4000/events", event).catch((err) => {
+    console.log(err);
+  });
+  axios.post("http://localhost:4001/events", event).catch((err) => {
+    console.log(err);
+  });
+  axios.post("http://localhost:4002/events", event).catch((err) => {
+    console.log(err);
+  });
+
+  res.send({ status: "OK" });
+});
+
+app.listen(4005, () => {
+  console.log("listening on 4005");
+});
